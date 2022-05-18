c20d90bf0c683a6453b62916cbf1e71ffe687847
AnishN456 <104634494+AnishN456@users.noreply.github.com>
2022-05-19 00:08:55 +0530

    Add files via upload

---
+++ random_float.py
@@ -0,0 +1,6 @@
+import random
+random_number=random.randint(0,5)
+
+random_float=random.random()
+random_floating_point=5*random_float
+print(random_floating_point)
---
+++ random_module.py
@@ -0,0 +1,7 @@
+import random
+import my_module
+
+#randint() --> will generate random numbers between a and b and including both numbers.
+random_integer = random.randint(1,10)
+print(random_integer)
+print(random_integer*my_module.pi)
